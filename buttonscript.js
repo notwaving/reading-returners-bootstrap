26cbd7403b2811b418b5eab697dc8152b3e71182
Samantha Burke <samanthaburke.uk@gmail.com>
2020-04-06 14:50:22 +0100

    customises popup box messages

--- buttonscript.js
+++ buttonscript.js
@@ -1,11 +1,20 @@
-function myFunction() { 
-  alert("I am an alert box!"); 
-};
+// When user clicks the finished button, they are asked to leave a review
 
-function userPreference() {
-  if(confirm("Are you sure you want to delete this book?") == true){
-    userPreference = "Book deleted";
-  } else {
-    userPreference = "Delete cancelled"
+function userFinishedBook() {
+  let userReview = prompt("Congratulations on finishing (book title). \n Would you like to leave a review?");
+  if (userReview == null || userReview == "") {
+    userReview = "Book archived only";
+  } else { 
+    userReview = "Book reviewed and archived"
   }
+}
+
+// When user clicks the delete button they are asked to confirm or cancel this choice
+function userDeleteBook() {
+  if(confirm("Are you sure you want to delete this book?") == true){
+    userDeleteBook = "Book deleted";
+  } else {
+    userDeleteBook = "Delete cancelled";
+  } 
 }  
+
