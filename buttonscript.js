227e3307d113cf078ec9e087c6481851bc1140ce
Samantha Burke <samanthaburke.uk@gmail.com>
2020-04-06 16:00:06 +0100

    refactors all popup box functions in buttonscript.js

--- buttonscript.js
+++ buttonscript.js
@@ -1,24 +1,12 @@
 // When user clicks the finished button, they are asked to leave a review, using a prompt box
 
-function userFinishedBook() {
-  let userFinishedBook = prompt("Congratulations on finishing {book title}. \n Would you like to leave a review?");
-  if (userFinishedBook == null || userFinishedBook == "") {
-    userFinishedBook = "Book archived only";
-  } else { 
-    userFinishedBook = "Book reviewed and archived"
-  }
-}
+userFinishedBook = () => prompt("Congratulations on finishing {book title}.\nWould you like to leave a review?") ? 
+userFinishedBook == null || userFinishedBook == "" (userFinishedBook = "Book archived only")  : 
+userFinishedBook = "Book reviewed and archived";
+
 
 // When user clicks the delete button they are asked to confirm or cancel this choice, using a confirm box
-function userDeleteBook() {
-  if(confirm("Are you sure you want to delete this book?") == true){
-    userDeleteBook = "Book deleted";
-  } else {
-    userDeleteBook = "Delete cancelled";
-  } 
-}  
+userDeleteBook = () => confirm("Are you sure you want to delete this book?") ? userDeleteBook = "Book deleted" : userDeleteBook = "Delete cancelled";
 
 // When user clicks the review button, they're shown their review via an alert box
-function userBookReview() {
-  alert("{Displays user submitted book review from database}");
-}
+userBookReview = () => alert("{Displays user submitted book review from database}");
